b99197da91ed2ae3c949bea5e8bc722ac85428e4
Gabriel Rodriguez <gabriel.asticonsultoria@gmail.com>
2023-05-05 09:48:28 -0600

    Correeccion de Paquete

--- src/main/java/org/uv/practica09/Practica09Application.java
+++ src/main/java/org/uv/practica09/Practica09Application.java
@@ -1,13 +1,11 @@
-package org.uv.Practica09;
+package org.uv.practica09;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Practica09Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Practica09Application.class, args);
 	}
-
 }
